ad6bd04685cab2d8b985e85e818544046932faac
Ygor Guilherme <ygorfsguilherme@gmail.com>
2023-07-18 12:29:07 -0300

    init project

---
+++ src/main/java/com/projetoacademy/geekverse/GeekverseApplication.java
@@ -0,0 +1,13 @@
+package com.projetoacademy.geekverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoacademy/geekverse/GeekverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoacademy.geekverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
